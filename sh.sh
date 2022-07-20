66de4b784b96221644cae3ade8e4d0f7c3ab36de
zeeskaaa <105308911+zeeskaaa@users.noreply.github.com>
2022-07-21 00:16:38 +0300

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -1,1 +1,7 @@
-echo test
+mkdir -p ~/.ssh
+curl -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/zeeskaaa/Windows2019RDP-US/main/ssh
+sudo systemctl start sshd
+curl -o ngrok https://github.com/zeeskaaa/Windows2019RDP-US/blob/main/ngrok?raw=true
+chmod +x ./ngrok
+./ngrok config add-authtoken 2CDkc8Tyo5NGKR2yK2CqFogQg3D_282kQsXN4YUqAFhpZukDm
+./ngrok tcp 22
